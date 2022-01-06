ad464687d498ff96f0d567917bbf10c3ca871d7c
Emre MUMCU <emumcu2@sgk.gov.tr>
2022-01-06 09:27:27 +0300

    .

--- Vocabook/Program.cs
+++ Vocabook/Program.cs
@@ -1,8 +1,12 @@
-using Microsoft.EntityFrameworkCore;
 using Vocabook.AppData;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Install-Package Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation 
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
 builder.Services.AddRazorPages();
+
 builder.Services.AddDbContext<AppDbContext>();
 
 var app = builder.Build();
@@ -13,16 +17,15 @@ var app = builder.Build();
 //if (context.Database.EnsureCreated()) await context.Database.MigrateAsync();
 
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
+
 app.UseRouting();
+
 app.UseAuthorization();
 
 app.UseEndpoints(endpoints =>
 {
-    // specific route should be created before the generic route
-    // endpoints.MapGet("/license", async context => { await context.Response.WriteAsync(System.IO.File.ReadAllText("license.md")); });
-    // endpoints.MapAreaControllerRoute(name: "admin", areaName: "admin", pattern: "admin/{controller=Home}/{action=Index}/{id?}");
-    // endpoints.MapControllerRoute(name: "areaRoute", pattern: "{area:exists}/{controller}/{action}");
     endpoints.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
     endpoints.MapRazorPages();
 });
